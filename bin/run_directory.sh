0471c61e78bc7e2e5fb744ac761f398d3597b181
akaipain <akaipain@umich.edu>
2021-05-26 10:50:05 -0400

    include run_directory bash script

---
+++ bin/run_directory.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+mkdir -p "/Users/alex/workspace/cern/Bisa/output_results"
+
+rm -rf "output"
+
+for file in "$1/*.dat"; do
+    /Users/alex/workspace/cern/Bisa/bin/Debug-macosx-x86_64/Commissioning/Commissioning /Users/alex/workspace/cern/Bisa/data/config.json "$file"
+    file1="${file##*/}"
+    mv /Users/alex/workspace/cern/Bisa/output /Users/alex/workspace/cern/Bisa/output_results/"${file1%%.*}"
+done
